c8e4ed1d8e7c39ad291ee3f38fd67b64a88ac183
Denys Lahoda <denys.lahoda@itrexgroup.com>
2021-11-08 16:36:29 +0200

    Add css baseline

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
+import CssBaseline from '@mui/material/CssBaseline';
 
 export class App extends Component {
   render() {
     return (
       <div>
+        <CssBaseline />
         App
       </div>
     );
