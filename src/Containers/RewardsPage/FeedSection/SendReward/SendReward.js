7fedbd94649cf6f8edb46a3ac7613a41d4181016
Denys Lahoda <denys.lahoda@itrexgroup.com>
2021-11-10 15:56:31 +0200

    Added basic responsive styles

--- src/Containers/RewardsPage/FeedSection/SendReward/SendReward.js
+++ src/Containers/RewardsPage/FeedSection/SendReward/SendReward.js
@@ -12,7 +12,7 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  width: [320, 400],
   bgcolor: "background.paper",
   boxShadow: 12,
   p: 4,
